933b2509a418c322899485fe0b088c66f8d9b916
Ming Ming <mint2648@gmail.com>
2015-07-03 03:38:10 +0800

    Save image instead of store as text

--- src/sc_studio/camera_view.py
+++ src/sc_studio/camera_view.py
@@ -8,6 +8,7 @@ Refer to LICENSE for details
 
 import binascii
 import logging
+import os
 from PIL import Image, ImageTk
 import time
 import tkinter
@@ -33,7 +34,8 @@ class CameraView(View):
 
 		self._tk.protocol("WM_DELETE_WINDOW", self.on_press_close)
 
-		self._file = open("camera_" + str(int(time.time() * 1000)), "w")
+		self._base_dir = "camera_" + str(int(time.time() * 1000))
+		os.makedirs(self._base_dir)
 
 	def run(self):
 		super(CameraView, self).run()
@@ -42,22 +44,19 @@ class CameraView(View):
 	def on_new_input(self):
 		try:
 			hex_str = self.get_input()
-			text = self._get_display_list(hex_str)
-			bmp = self._get_bitmap(hex_str)
+			img = self._get_image(hex_str)
 		except Exception as e:
 			logging.debug(str(e))
 			return
-		if bmp is None:
+		if img is None:
 			return
 
+		bmp = ImageTk.BitmapImage(image = img, foreground = "black",
+				background = "white")
 		self._canvas.create_image(0, 0, image = bmp, anchor = tkinter.NW)
-		self._image = bmp
+		self._tk_image = bmp
 
-		self._file.write(time.strftime("[%x %X]\n"))
-		for line in text:
-			string = line.decode("UTF-8")
-			self._file.write(string)
-			self._file.write('\n')
+		img.save(self._base_dir + '/' + str(int(time.time() * 1000)) + ".png")
 
 	def on_dismiss(self):
 		self._tk.after_idle(self.on_press_close)
@@ -66,7 +65,7 @@ class CameraView(View):
 		self._tk.destroy()
 		self.join_io_thread()
 
-	def _get_bitmap(self, hex_str):
+	def _get_image(self, hex_str) -> Image:
 		try:
 			hex_data = binascii.unhexlify(hex_str)
 		except TypeError as e:
@@ -75,27 +74,4 @@ class CameraView(View):
 
 		img = Image.frombytes(mode = '1', size = (80, 60), data = hex_data)
 		img = img.resize((80 * self._multiplier, 60 * self._multiplier))
-		bmp = ImageTk.BitmapImage(image = img, foreground = "black",
-				background = "white")
-		return bmp
-
-	def _get_display_list(self, hex_str):
-		try:
-			hex_data = binascii.unhexlify(hex_str)
-		except TypeError as e:
-			logging.debug(str(e))
-			return None
-
-		byte_pos = 0
-		bit_pos = 0
-		display = [bytearray(b' ' * 160) for i in range(60)]
-		for i in range(60):
-			for j in range(80):
-				if hex_data[byte_pos] & (0x80 >> bit_pos):
-					display[i][j * 2] = ord('#');
-					display[i][j * 2 + 1] = ord('#');
-				bit_pos += 1
-				if bit_pos >= 8:
-					bit_pos = 0
-					byte_pos += 1
-		return display
+		return img
